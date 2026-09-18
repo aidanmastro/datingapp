271bf6b1670bcec26308de0c230ee2317a31c93e
Aidan Mastro-Battista <39303193+aidanmastro@users.noreply.github.com>
2026-09-18 16:41:51 +1000

    Fixes

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -9,7 +9,6 @@ import { User } from '../../types/user';
   templateUrl: './home.html',
 })
 export class Home {
-  @Input({required: true}) membersFromApp: User[] = [];
   protected registerMode = signal(false);
 
   showRegister(value: boolean) {
